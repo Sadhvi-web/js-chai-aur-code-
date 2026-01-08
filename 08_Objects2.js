6ce12d23b5e2c8460fd8049537a07e711d13a39a
Sadhvi-web <kumarisadhvi01@gmail.com>
2026-01-08 15:09:26 +0000

    Destructuring

---
+++ 08_Objects2.js
@@ -0,0 +1,13 @@
+// Destructuring in objects
+const course = {
+    coursename : "JS in hindi",
+    courseprice : 2000,
+    coursechannel : "Chai aur code"
+}
+
+// console.log(course.coursename);      //Instead of using this long way we hacve to do destructuring
+const {coursename} = course
+console.log(coursename);
+const {coursename:Cname} = course
+console.log(Cname);
+
